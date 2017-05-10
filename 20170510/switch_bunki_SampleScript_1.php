702bb1ee740e4ce56ea0dff0dcb334c96cc7feb3
mklpij21985 <mklpij21985@yahoo.co.jp>
2017-05-10 13:51:23 +0900

    switch script commit

---
+++ 20170510/switch_bunki_SampleScript_1.php
@@ -0,0 +1,24 @@
+<?php
+
+$level1 = "A";
+
+switch ($level1)
+{
+    case "A";
+        print ("すばら!");
+        break;
+
+    case "B";
+        print ("いいね");
+        break;
+
+    case "C";
+        print ("悪くない");
+        break;
+
+    case "D";
+        print ("がんばって");
+        break;
+}
+
+?>
