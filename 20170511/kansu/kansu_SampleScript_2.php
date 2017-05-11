b830508a06edf8859826040065614677a3f696d8
mklpij21985 <mklpij21985@yahoo.co.jp>
2017-05-11 11:55:27 +0900

    kansu script commit

---
+++ 20170511/kansu/kansu_SampleScript_2.php
@@ -0,0 +1,12 @@
+<?php
+
+    function familyName($fname)
+    {
+        echo "{$fname} yamada.  <br>";
+    }
+familyName("yuuki");
+familyName("hirobumi");
+familyName("yukiko");
+familyName("hanako");
+familyName("keichi");
+?>
---
+++ 20170511/kansu/kansu_SampleScript_4.php
@@ -0,0 +1,11 @@
+<?php
+
+    function setHeight($minheight = 50)
+    {
+        echo "高さは{$minheight} <br>";
+    }
+setHeight("150,000,000");
+setHeight();
+setHeight(135);
+setHeight(80);
+?>
